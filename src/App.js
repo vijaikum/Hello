4a68d56e9c169838577ae529f5dd0cc30b035340
Vijai <vijaithedude@gmail.com>
2020-05-13 15:55:07 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/page1" component={Page1} />
+          <Route path="/page2" component={Page2} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+// Home.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="page page1">
+      <div className="flagTop" />
+      <div className="flagCenter">
+        <h1 className="country">Home Page</h1>
+        <div className="otherLinks">
+          <Link to="/page1">Argentina</Link>
+          <Link to="/page2">Nigeria</Link>
+        </div>
+      </div>
+      <div className="flagBottom" />
+    </div>
+  );
+};
+
+export default Home;
